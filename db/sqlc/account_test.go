c39e78a28fc5ec0aeb2922b7b9f4762e17f1f085
toch <tocha.mateusz+main@gmail.com>
2023-11-24 11:27:04 +0100

    feat: create test suits for accounts

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,26 +2,88 @@ package simple_bank
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func TestCreateAccount(t *testing.T) {
+type AccountSuite struct {
+	suite.Suite
+	db          *gorm.DB
+	account     Account
+	testQueries *Queries
+}
+
+func TestAccountSuite(t *testing.T) {
+
+	dns := fmt.Sprintf("host=0.0.0.0 user=root password=password_test dbname=simple_bank_test port=5432 sslmode=disable")
+
+	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		log.Fatal("cannot connect to DB", err)
+	}
+
+	testingSuit := new(AccountSuite)
+	testingSuit.db = db
+	testingSuit.testQueries = New(testingSuit.db.ConnPool)
+
+	suite.Run(t, testingSuit)
+
+}
+
+func (s *AccountSuite) SetupSuite() {
+	log.Println("SetupSuite()")
+}
+
+func (s *AccountSuite) TearDownSuite() {
+	log.Println("TearDownSuite()")
+	s.db.Raw("TRUNCATE accounts CASCADE")
+}
+
+// run before each test
+func (s *AccountSuite) BeforeTest(suiteName, testName string) {
+	log.Println("BeforeTest()", suiteName, testName)
+}
+
+// run after each test
+func (s *AccountSuite) AfterTest(suiteName, testName string) {
+	log.Println("AfterTest()", suiteName, testName)
+}
+
+func (s *AccountSuite) TestCreateAccount() {
 	arg := CreateAccountParams{
 		Owner:    "tom",
 		Balance:  100,
 		Currency: "USD",
 	}
+	s.T()
 
-	account, err := testQueries.CreateAccount(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
+	account, err := s.testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(s.T(), err)
+	require.NotEmpty(s.T(), account)
 
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Currency, account.Currency)
+	require.Equal(s.T(), arg.Owner, account.Owner)
+	require.Equal(s.T(), arg.Balance, account.Balance)
+	require.Equal(s.T(), arg.Currency, account.Currency)
 
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
+	require.NotZero(s.T(), account.ID)
+	require.NotZero(s.T(), account.CreatedAt)
+
+	s.account = account
+}
+
+func (s *AccountSuite) TestGetAccount() {
+	resultAccount, err := s.testQueries.GetAccountByID(context.Background(), s.account.ID)
+	require.NoError(s.T(), err)
+
+	require.NotEmpty(s.T(), resultAccount)
+
+	require.Equal(s.T(), s.account.Currency, resultAccount.Currency)
+	require.Equal(s.T(), s.account.Balance, resultAccount.Balance)
+	require.Equal(s.T(), s.account.Owner, resultAccount.Owner)
 }
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,28 +1,13 @@
 package simple_bank
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"testing"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 var testQueries *Queries
 
 func TestMain(m *testing.M) {
-
-	dns := fmt.Sprintf("host=0.0.0.0 user=root password=password_test dbname=simple_bank_test port=5432 sslmode=disable")
-
-	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
-
-	if err != nil {
-		log.Fatal("cannot connect to DB", err)
-	}
-
-	testQueries = New(db.ConnPool)
 
 	os.Exit(m.Run())
 }
